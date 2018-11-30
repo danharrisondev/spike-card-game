fb03aea11d60db85fe892c2fcdd6977efb6ad25a
Daniel Harrison <harrison92d+orangudan@gmail.com>
2018-11-30 23:10:33 +0000

    Refactor

---
+++ javascripts/card.js
@@ -0,0 +1,19 @@
+((module) => {
+    
+    module.render = function(props) {
+        const friendlyTypeNames = {
+            'generator': 'Science Generator',
+            'tool': 'Tool',
+        };
+
+        return `<div class="card card-${props.type}">
+                    <div class="title">${props.name}</div>
+                    <div class="image-container">
+                        <img src="${props.image}" />
+                    </div>
+                    <div class="type">${friendlyTypeNames[props.type]}</div>
+                    <div class="description">${props.description}</div>
+                </div>`;
+    };
+
+})(window.Card = window.Card || {});
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,43 +1,8 @@
-function card(type, name, description, image) {
-    const friendlyTypeNames = {
-        'generator': 'Science Generator',
-        'tool': 'Tool'
-    };
-
-    return {
-        type,
-        name,
-        description,
-        html: function() {
-            return `<div class="card card-${type}">
-                        <div class="title">${name}</div>
-                        <div class="image-container">
-                            <img src="${image}" />
-                        </div>
-                        <div class="type">${friendlyTypeNames[type]}</div>
-                        <div class="description">${description}</div>
-                    </div>`;
-        }
-    }
-}
-
-function generator(name, description, image) {
-    return card('generator', name, description, image);
-}
-
-function tool(name, description, image) {
-    return card('tool', name, description, image);
-}
-
 const cards = [
-    generator('Space Shuttle', 'Basic ship built by USA to get to the ISS.', 'https://www.nasa.gov/sites/default/files/thumbnails/image/shuttle_launch.jpg'),
-    tool('Electromagnetic Pulse Bomb', 'Disables the target player\'s science generation for 3 turns.', 'https://www.extremetech.com/wp-content/uploads/2013/11/emp-blast-640x353.jpg'),
+    { type: 'generator', name: 'Space Shuttle', description: 'Basic ship built by USA to get to the ISS.', image: 'https://www.nasa.gov/sites/default/files/thumbnails/image/shuttle_launch.jpg' },
+    { type: 'tool', name: 'Electromagnetic Pulse Bomb', description: 'Disables the target player\'s science generation for 3 turns.', image: 'https://www.extremetech.com/wp-content/uploads/2013/11/emp-blast-640x353.jpg' },
 ];
 
 const cardContainer = document.getElementById('cardContainer');
 
-cards.forEach(card => {
-    var elementToRender = document.createElement('div');
-    elementToRender.innerHTML = card.html();
-    cardContainer.appendChild(elementToRender);
-});
+cardContainer.innerHTML = cards.map(card => Card.render(card)).join('');
