5864f8ad9c779e86b89e56f9c3726e5e733f3079
Daniel Harrison <harrison92d+orangudan@gmail.com>
2018-11-30 23:24:22 +0000

    Whitespace to separate generators and tools

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cards = [
     { type: 'generator', name: 'Space Shuttle', description: 'Basic ship built by USA to get to the ISS.', image: 'https://www.nasa.gov/sites/default/files/thumbnails/image/shuttle_launch.jpg' },
     { type: 'generator', name: 'International Space Station', description: 'Big station in the sky. Sometimes visible from Earth.', image: 'https://img.purch.com/w/660/aHR0cDovL3d3dy5zcGFjZS5jb20vaW1hZ2VzL2kvMDAwLzA1OS83Nzkvb3JpZ2luYWwvc3BhY2Vfc3RhdGlvbl9vdmVyX2VhcnRoLmpwZw==' },
+    
     { type: 'tool', name: 'Electromagnetic Pulse Bomb', description: 'Disables the target player\'s science generation for 3 turns.', image: 'https://www.extremetech.com/wp-content/uploads/2013/11/emp-blast-640x353.jpg' },
     { type: 'tool', name: 'Solar Storm', description: 'A solar storm disables all player\'s research technologies for X turns.', image: 'https://cdn.images.express.co.uk/img/dynamic/151/590x/Solar-storm-1026400.jpg?r=1538663877518' },
     { type: 'tool', name: 'Meteor', description: 'Destroys all Earth-bound cards on the field of play.', image: 'https://thenypost.files.wordpress.com/2018/08/180806-air-force-quiet-after-asteroid-feature.jpg?quality=90&strip=all&w=618&h=410&crop=1' },
